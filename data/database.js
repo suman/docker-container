3f3d5ac6bd9c30a2500e62da7b9bd65c5c867533
suman <sumanbogati@gmail.com>
2023-11-18 18:48:42 +0530

    Modify the file

--- data/database.js
+++ data/database.js
@@ -9,7 +9,7 @@ const dbName = process.env.MONGODB_DB_NAME;
 const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
-console.log('Trying to connect to db');
+console.log('Trying to connect to db ');
 
 try {
   await client.connect();
